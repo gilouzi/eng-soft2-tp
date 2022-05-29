08aca53d9172dad96dbec140027893f882873919
ThiagoPoppe <thiagopoppe@dcc.ufmg.br>
2022-05-29 10:53:43 -0300

    Resolvendo conflitos na main

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import json
+import argparse
+
 from flask import Flask
 from flask import render_template
 from flask import url_for, redirect
@@ -6,6 +9,8 @@ from datetime import timedelta
 from ecommerce.models.product import db_product
 from ecommerce.models.shopping_cart import ShoppingCart
 from ecommerce.models.user import db_user
+from ecommerce.models.product import Product, ProductCategory
+
 
 app = Flask(__name__)
 app.secret_key = "eng-soft2-tp"
@@ -16,15 +21,35 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db_product.init_app(app)
 user_cart = ShoppingCart(None)
 
+
 @app.before_first_request
 def create_database():
     db_product.create_all()
     db_user.create_all()
 
+
 @app.route('/')
 def home():
     return render_template('index.html')
-    
+
+
+def parse_command_line_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--default-db', dest='create_default_db', action='store_true', required=False, help='flag to create a default database')
+
+    args = parser.parse_args()
+    return args
+
+
+def create_default_database():
+    with open('default_products.json', 'r') as f:
+        products = json.load(f)
+
+    for product in products:
+        product['category'] = ProductCategory[product['category']]
+        p = Product(**product)
+        
+
 
 # import declared routes
 import ecommerce.api.product
@@ -33,4 +58,9 @@ import ecommerce.api.user
 
 
 if __name__ == '__main__':
+    args = parse_command_line_arguments()
+    if args.create_default_db:
+        print('[warning] Creating default database...')
+        create_default_database()
+
     app.run(debug=True)
