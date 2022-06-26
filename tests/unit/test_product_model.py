92685d043d85c407b39eefe121f51c97d1823015
Thiago Martin Poppe <thiagopoppe@dcc.ufmg.br>
2022-06-26 13:25:02 -0300

    Criando fixture para deixar os testes mais limpos

--- tests/unit/test_product_model.py
+++ tests/unit/test_product_model.py
@@ -3,31 +3,39 @@ from ecommerce.api.exceptions import UndefinedProductCategoryError
 from ecommerce.models.product import Product, ProductCategory
 
 class TestProductModel(unittest.TestCase):
+    def setUp(self) -> None:
+        self.product = Product('test product', 0, 0, None, 0, 'test product')
 
     def test_food_shipping_price(self):
-        product = Product("food product", 100, 12, ProductCategory.FOOD, 10, "test food")
-        self.assertEqual(5, product.get_shipping_price())
+        self.product.price = 100
+        self.product.category = ProductCategory.FOOD
+        self.assertEqual(5, self.product.get_shipping_price())
 
     def test_utensil_shipping_price(self):
-        product = Product("utensil product", 100, 12, ProductCategory.UTENSIL, 10, "test utensil")
-        self.assertEqual(12, product.get_shipping_price())
+        self.product.price = 100
+        self.product.category = ProductCategory.UTENSIL
+        self.assertEqual(12, self.product.get_shipping_price())
     
     def test_eletronic_shipping_price(self):
-        product = Product("eletronic product", 100, 12, ProductCategory.ELETRONIC, 10, "test eletronic")
-        self.assertEqual(35, product.get_shipping_price())
+        self.product.price = 100
+        self.product.category = ProductCategory.ELETRONIC
+        self.assertEqual(35, self.product.get_shipping_price())
     
     def test_raise_undefined_product_category_error(self):
-        product = Product("undefined product", 100, 12, "UNDEFINED", 10, "test undefined")
-        self.assertRaises(UndefinedProductCategoryError, product.get_shipping_price)
+        self.product.category = 'UNDEFINED'
+        self.assertRaises(UndefinedProductCategoryError, self.product.get_shipping_price)
 
     def test_food_shipping_price_rouding(self):
-        product = Product("food product", 1.50, 12, ProductCategory.FOOD, 10, "test food")
-        self.assertEqual(0.08, product.get_shipping_price())
+        self.product.price = 1.50
+        self.product.category = ProductCategory.FOOD
+        self.assertEqual(0.08, self.product.get_shipping_price())
     
     def test_utensil_shipping_price_rouding(self):
-        product = Product("utensil product", 1.55, 12, ProductCategory.UTENSIL, 10, "test utensil")
-        self.assertEqual(0.19, product.get_shipping_price())
+        self.product.price = 1.55
+        self.product.category = ProductCategory.UTENSIL
+        self.assertEqual(0.19, self.product.get_shipping_price())
 
     def test_eletronic_shipping_price_rounding(self):
-        product = Product("eletronic product", 1.50, 12, ProductCategory.ELETRONIC, 10, "test eletronic")
-        self.assertEqual(0.53, product.get_shipping_price())
+        self.product.price = 1.50
+        self.product.category = ProductCategory.ELETRONIC
+        self.assertEqual(0.53, self.product.get_shipping_price())
